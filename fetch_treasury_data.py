a43eefadcf490acfd5b7ad438a5373037630586a
Julian Martinez <julianmartinez@Julians-MBP.lan1>
2024-04-15 22:00:36 -0700

    saves fetched data to subdirectory

--- fetch_treasury_data.py
+++ fetch_treasury_data.py
@@ -1,5 +1,13 @@
 import requests
 import pandas as pd
+import os
+
+# Define the subdirectory path
+subdirectory = 'fetched-data'
+
+# Check if the subdirectory exists, and if not, create it
+if not os.path.exists(subdirectory):
+    os.makedirs(subdirectory)
 
 def get_treasury_data(start_year, end_year, field):
     base_url = "https://api.fiscaldata.treasury.gov/services/api/fiscal_service/"
@@ -23,14 +31,16 @@ def save_data_to_csv(data, start_year, current_year):
         df = pd.DataFrame(data['data'])
         # Construct filename with start_year and current_year included
         filename = f"treasury_data_{start_year}_to_{current_year}.csv"
-        df.to_csv(filename, index=False)
-        print(f"Data successfully saved to {filename}")
+        # Update file path to include the subdirectory
+        file_path = os.path.join(subdirectory, filename)
+        df.to_csv(file_path, index=False)
+        print(f"Data successfully saved to {file_path}")
     else:
         print("No data available to save")
 
 # Example usage
-start_year = 2022
-current_year = 2023  # Update this as needed
+start_year = 2023
+current_year = 2024  # Update this as needed
 field = "bid_to_cover_ratio"
 data = get_treasury_data(start_year, current_year, field)
 
